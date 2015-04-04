8b249fa70d8f8dc53ef2cc489819c454e4faa0d7
Arthur Clemens <arthurclemens@gmail.com>
2015-04-04 02:44:24 +0200

    add build option

--- Gruntfile.js
+++ Gruntfile.js
@@ -18,6 +18,8 @@ module.exports = function(grunt) {
                 }
             }
         },
+
+        // symlink: only for local development of Polythene
         symlink: {
             options: {
                 overwrite: false
@@ -33,9 +35,41 @@ module.exports = function(grunt) {
                     }
                 ]
             }
+        },
+
+        // build options
+        // see: https://github.com/cloudchen/requirejs-bundle-examples
+        requirejs: {
+            options: {
+                'appDir': 'src',
+                'dir': 'build',
+                'mainConfigFile': 'src/common.js',
+                'optimize': 'uglify2',
+                'normalizeDirDefines': 'skip',
+                'skipDirOptimize': true,
+            },
+            shared: {
+                options: {
+                    'modules': [
+                        {
+                            'name': 'common',
+                            'include': ['css']
+                        },
+                        {
+                            'name': 'app/icon/main',
+                            'exclude': ['common']
+                        },
+                        {
+                            'name': 'app/header-panel/main',
+                            'exclude': ['common']
+                        }
+                    ]
+                }
+            }
         }
     });
     grunt.loadNpmTasks('grunt-bowercopy');
     grunt.loadNpmTasks('grunt-contrib-symlink');
+    grunt.loadNpmTasks('grunt-requirejs');
     grunt.registerTask('default', ['bowercopy']);
 };
